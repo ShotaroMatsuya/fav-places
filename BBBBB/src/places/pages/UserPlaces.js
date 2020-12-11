13521a609924c3f911969b35d04eadc304346455
ShotaroMatsuya <matsuya@h01.itscom.net>
2020-12-11 15:47:19 +0900

    creating UI (Modal & Button)& implement transition animation from react-transition-group

---
+++ BBBBB/src/places/pages/UserPlaces.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useParams } from 'react-router-dom'; //useParamsはfunctional component内で使用できる
+
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Tokyo Tower',
+    description: 'One of the most famous tourist spot in Japan!',
+    imageUrl:
+      'https://d2hpum9hu56in0.cloudfront.net/userimage/photo/image/17267/large_original_aspect_for_pc_375f6b7f-d6d1-4615-a302-1f22fc95e884.jpeg',
+    address: '〒105-0011 東京都港区芝公園４丁目２−８',
+    location: {
+      lat: 35.6585805,
+      lng: 139.7432442,
+    },
+    creator: 'u1',
+  },
+  {
+    id: 'p2',
+    title: 'Tokyo Tower',
+    description: 'One of the most famous tourist spot in Japan!',
+    imageUrl:
+      'https://d2hpum9hu56in0.cloudfront.net/userimage/photo/image/17267/large_original_aspect_for_pc_375f6b7f-d6d1-4615-a302-1f22fc95e884.jpeg',
+    address: '〒105-0011 東京都港区芝公園４丁目２−８',
+    location: {
+      lat: 35.6585805,
+      lng: 139.7432442,
+    },
+    creator: 'u2',
+  },
+];
+
+const UserPlaces = () => {
+  //useParamsはurlクエリパラメータをオブジェクトとして取得する事ができる
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;
--- BBBBB/src/shared/components/Navigation/SideDrawer.js
+++ BBBBB/src/shared/components/Navigation/SideDrawer.js
@@ -16,7 +16,9 @@ const SideDrawer = props => {
       unmountOnExit
       //   mountOnEnterはinがtrueになるまでマウントしなかったり、unmountOnExitはexitedになる度アンマウントする為のプロパティです。消える時はアニメーションしなくていいみたいな場合に使うといい感じです。またonEnteredやonExitedなどを渡して完了した時に連動してある処理を行うこともできます。
     >
-      <aside className="side-drawer">{props.children}</aside>
+      <aside className="side-drawer" onClick={props.onClick}>
+        {props.children}
+      </aside>
     </CSSTransition>
   );
   return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
