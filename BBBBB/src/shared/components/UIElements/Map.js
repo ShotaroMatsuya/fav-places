8b21d0d4af99ff1333bc53544a8f8ae7f375a92f
ShotaroMatsuya <matsuya@h01.itscom.net>
2020-12-13 02:33:11 +0900

    displaying googleMap

--- BBBBB/src/shared/components/UIElements/Map.js
+++ BBBBB/src/shared/components/UIElements/Map.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+
+import './Map.css';
 
 const Map = props => {
-  return <div></div>;
+  //DOM要素を取得するためにreactではuseRefを使用するのがsmart
+  const mapRef = useRef();
+
+  //jsxがrenderingされたあとに実行(useRefがjsxコードestablishされてから実行されないといけない)
+  const { center, zoom } = props;
+  useEffect(() => {
+    //init google map api(where should it be rendered)
+    const map = new window.google.maps.Map(mapRef.current, {
+      center: center,
+      zoom: zoom,
+    });
+    //config marker position
+    new window.google.maps.Marker({
+      position: center,
+      map: map,
+    });
+  }, [center, zoom]);
+  return (
+    <div
+      //useRefの返り値をsetしてbind
+      ref={mapRef}
+      className={`map ${props.className}`}
+      style={props.style}
+    ></div>
+  );
 };
 export default Map;
