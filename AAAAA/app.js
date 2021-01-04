d5b26e669fae0abc5675f4a44aa895816040a7e9
ShotaroMatsuya <matsuya@h01.itscom.net>
2021-01-04 17:37:52 +0900

    adding some API endpoint about places and users

--- AAAAA/app.js
+++ AAAAA/app.js
@@ -2,10 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
+//post-requestを受け取るときに必要
+//jsonメソッドを使うと、reqに含まれるjsonファイルをjavascriptのobjectやarrayに変形してくれる
+app.use(bodyParser.json());
+
 app.use('/api/places', placesRoutes); //routeオブジェクトはexpressにより自動的にmiddlewareとして扱われる(nextいらない)
+app.use('/api/users', usersRoutes);
+//router middlewareのあとに404エラー用middlewareを作成
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error; //synchronousなのでthrow
+});
 
 //error handling middleware...4つのparameterをセットするとexpressにより自動的にerror middlewareと認識される
 app.use((error, req, res, next) => {
--- AAAAA/controllers/places-controllers.js
+++ AAAAA/controllers/places-controllers.js
@@ -1,6 +1,8 @@
+const { v4: uuidv4 } = require('uuid');
+
 const HttpError = require('../models/http-error');
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: 'p1',
     title: 'Station of Tokyo',
@@ -27,18 +29,57 @@ const getPlaceById = (req, res, next) => {
   res.json({ place }); // meant by modern js syntax => {place} => {place:place}
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
-  const place = DUMMY_PLACES.find(p => {
+  const places = DUMMY_PLACES.filter(p => {
     return p.creator === userId;
   });
-  if (!place) {
+
+  if (!places || places.length === 0) {
     return next(
-      new HttpError('Could not find a place for the provided user id.', 404)
+      new HttpError('Could not find a places for the provided user id.', 404)
     );
   }
-  res.json({ place });
+  res.json({ places });
+};
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  const createdPlace = {
+    id: uuidv4(), //uuidパッケージよりインストール、uniqueなidを自動生成する
+    title, //{title:title}のショートハンド
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+
+  DUMMY_PLACES.push(createdPlace); //unshift(createdPlace)だと先頭に追加
+  res.status(201).json({ place: createdPlace });
+};
+
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  //まずはimmutableにobjectやarrayを操作(cloneを作成)してから最後に上書きするのがbest practice
+  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) }; //spread-operatorでcloneを作成
+  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({ place: updatedPlace });
+};
+const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
+
+  res.status(200).json({ message: 'Deleted place.' });
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
+exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
