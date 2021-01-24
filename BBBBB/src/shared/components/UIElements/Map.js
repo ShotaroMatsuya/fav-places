87bfaf1d6c024d984d27fd83d4f14326febeefca
ShotaroMatsuya <matsuya@h01.itscom.net>
2021-01-24 19:21:58 +0900

    implement sending http request of signup & login from client-side

--- BBBBB/src/shared/components/UIElements/Map.js
+++ BBBBB/src/shared/components/UIElements/Map.js
@@ -3,10 +3,11 @@ import React, { useRef, useEffect } from 'react';
 import './Map.css';
 
 const Map = props => {
-  //DOM要素を取得するためにreactではuseRefを使用するのがsmart
+  //(1)DOM要素を取得するためにreactではuseRefを使用するのがsmart
+  // (2)useRefにはrender後もsurviveする変数を定義することができる
   const mapRef = useRef();
 
-  //jsxがrenderingされたあとに実行(useRefがjsxコードestablishされてから実行されないといけない)
+  //jsxがrenderingされたあとに実行(useRefがjsxコードestablishされてから実行されないといけない)->useEffectを使う
   const { center, zoom } = props;
   useEffect(() => {
     //init google map api(where should it be rendered)
